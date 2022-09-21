1959b35a54d5e399f890436a83300d014505920c
J.Endurium <J.Endurium.K@gmail.com>
2022-09-21 12:25:31 -0500

    Testing basic tweepy functions

---
+++ keyGetTest.py
@@ -0,0 +1,12 @@
+from configparser import RawConfigParser
+import tweepy
+config = RawConfigParser()
+config.read('../config/api_keys.cfg')
+
+#API_KEY = config.get('twitter','api_key')
+#API_KEY_SECRET = config.get('twitter','api_key_secret')
+BEARER_TOKEN = config.get('twitter','bearer_token')
+
+client = tweepy.Client(BEARER_TOKEN)
+thisUser = client.get_user(id=1148529339203543040)
+print(thisUser)

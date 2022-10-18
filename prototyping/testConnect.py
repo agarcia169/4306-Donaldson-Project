18b57b06fefcb29abef019305ae778ee8df84d36
JEnduriumK <91800518+JEnduriumK@users.noreply.github.com>
2022-10-18 09:46:33 -0500

    Included examples of cfg files

--- prototyping/testConnect.py
+++ prototyping/testConnect.py
@@ -1,16 +1,28 @@
-import ntpath
 import mysql.connector
 from mysql.connector import errorcode
 from configparser import RawConfigParser
-import os
 
 config = RawConfigParser()
-config.read('../config/server.cfg')
-config.read('../config/api_keys.cfg')
 
+server_file_location = '../config/server.cfg'
+# [mysql]
+# username=example
+# password=example
+# host=example
+# database=example
+# port=example
+api_key_file_location = '../config/api_keys.cfg'
+# [twitter]
+# API_KEY=example
+# API_KEY_SECRET=example
+# BEARER_TOKEN=example
+
+config.read(server_file_location)
+config.read(api_key_file_location)
 
 API_KEY = config.get('twitter','api_key')
 API_KEY_SECRET = config.get('twitter','api_key_secret')
+BEARER_TOKEN = config.get('twitter','bearer_token')
 USER= config.get('mysql','username')
 DATABASE = config.get('mysql','database')
 HOST = config.get('mysql','host')

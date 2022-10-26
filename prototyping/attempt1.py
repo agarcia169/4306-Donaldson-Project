7839fa4259fc58e64a251ca981d05d5580e54cad
Alexander Garcia <agarcia169@angelo.edu>
2022-10-26 11:21:08 -0500

    added add_tweet_to_database and get_tweet_from_database

--- prototyping/attempt1.py
+++ prototyping/attempt1.py
@@ -124,7 +124,60 @@ def get_twitter_id(dbConnection: MySQLConnection,twitter_handle:str) -> list[int
       return [item[0] for item in result]
   except mysql.connector.cursor.Error as cursorErr:
     print(cursorErr)
- 
+
+#LMAO good luck whether these work or not 
+def add_tweet_to_database(dbConnection: MySQLConnection,tweet:dict | requests.Response) -> tuple[bool,int]:
+    """Adds a Tweet ID and a Tweets Content
+
+  Args:
+      `dbConnection` (`MySQLConnection`): The open connection to the MySQL database.
+      `tweet` (`Dict`|`Response`): The output from the `tweepy.get_users_tweets()` function. Must contain \
+        `data.id`, `data.text`, and some other stuff that we probably dont need for now.
+
+  Returns:
+      `tuple(bool,int)`: A bool representing whether or not the tweet was added successfully, and an int that is the Unique ID# of Tweet
+  """
+    try:
+        with dbConnection.cursor() as dbCursor:
+          query_check_for_tweet_id = "SELECT id FROM tweets WHERE id = %s" #FIXME
+          dbCursor.execute(query_check_for_tweet_id,(tweet.data.id,))
+          do_they_exist = dbCursor.fetchall()
+          print(dir(do_they_exist))
+          print(do_they_exist)
+          if(do_they_exist == []):
+            query_add_tweet_to_db = "INSERT INTO tweets VALUES(%s,%s)" #FIXME
+            dbCursor.execute(query_add_tweet_to_db,(tweet.data.id,tweet.data.text,))
+            dbCursor.fetchall()
+            dbCursor.commit()
+            return (True,tweet.data.id)
+
+          else:
+            print("Tweet" + tweet.data.id + " Exists In Database Already")
+            return (False,do_they_exist[0][0])
+
+    except mysql.connector.cursor.Error as cursorErr:
+        print(cursorErr)
+
+def get_tweet_from_db(dbConnection: MySQLConnection,tweet_id:int|str) -> str:
+  """Gets a tweet from the DB of a provided Tweet ID#, `twitter_id`.
+  #add author functionality later
+  Args:
+      `dbConnection` (`MySQLConnection`): The connection object to the MySQL database
+      `tweet_id` (`int`|`str`): The Tweet ID#
+
+  Returns:
+      `str`: The first tweet returned by the database.
+  """
+  try:
+    with dbConnection.cursor() as dbCursor:
+      dbCursor.execute("SELECT id FROM tweets WHERE id = %s",(str(twitter_id),))
+      result = dbCursor.fetchall()
+      if(len(result) > 1):
+        print("Warning: Multiple user tweets returned with supposedly unique ID#%s"%tweet_id)
+      return result[0][0]
+  except mysql.connector.cursor.Error as cursorErr:
+    print(cursorErr)
+
 # End of Database Functions
 
 # Define Tweepy Functions Here

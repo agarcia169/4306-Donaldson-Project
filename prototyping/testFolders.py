17260871464c8736bd5bb1a5b67aaa18501c4e77
JEnduriumK <91800518+JEnduriumK@users.noreply.github.com>
2022-10-27 10:06:44 -0500

    fixes How do you import from a directory? #39

--- prototyping/testFolders.py
+++ prototyping/testFolders.py
@@ -1,33 +1,35 @@
-import .\testFolder\PythonTestFile
-import ./testFolder/PythonTestFile
-import PythonTestFile
-import ../prototyping/testFolder/PythonTestFile
-import ..\prototyping\testFolder\PythonTestFile
-import prototyping\testFolder\PythonTestFile
-import prototyping/testFolder/PythonTestFile
+# import .\testFolder\PythonTestFile
+# import ./testFolder/PythonTestFile
+# import PythonTestFile
+# import ../prototyping/testFolder/PythonTestFile
+# import ..\prototyping\testFolder\PythonTestFile
+# import prototyping\testFolder\PythonTestFile
+# import prototyping/testFolder/PythonTestFile
 
-import .\\testFolder\\PythonTestFile
-import .//testFolder//PythonTestFile
-import PythonTestFile
-import ..//prototyping//testFolder//PythonTestFile
-import ..\\prototyping\\testFolder\\PythonTestFile
-import prototyping\\testFolder\\PythonTestFile
-import prototyping//testFolder//PythonTestFile
+# import .\\testFolder\\PythonTestFile
+# import .//testFolder//PythonTestFile
+# import PythonTestFile
+# import ..//prototyping//testFolder//PythonTestFile
+# import ..\\prototyping\\testFolder\\PythonTestFile
+# import prototyping\\testFolder\\PythonTestFile
+# import prototyping//testFolder//PythonTestFile
 
-import ".\testFolder\PythonTestFile"
-import "./testFolder/PythonTestFile"
-import "PythonTestFile"
-import "../prototyping/testFolder/PythonTestFile"
-import "..\prototyping\testFolder\PythonTestFile"
-import "prototyping\testFolder\PythonTestFile"
-import "prototyping/testFolder/PythonTestFile"
+# import ".\testFolder\PythonTestFile"
+# import "./testFolder/PythonTestFile"
+# import "PythonTestFile"
+# import "../prototyping/testFolder/PythonTestFile"
+# import "..\prototyping\testFolder\PythonTestFile"
+# import "prototyping\testFolder\PythonTestFile"
+# import "prototyping/testFolder/PythonTestFile"
 
-import ".\\testFolder\\PythonTestFile"
-import ".//testFolder//PythonTestFile"
-import "PythonTestFile"
-import "..//prototyping//testFolder//PythonTestFile"
-import "..\\prototyping\\testFolder\\PythonTestFile"
-import "prototyping\\testFolder\\PythonTestFile"
-import "prototyping//testFolder//PythonTestFile"
+# import ".\\testFolder\\PythonTestFile"
+# import ".//testFolder//PythonTestFile"
+# import "PythonTestFile"
+# import "..//prototyping//testFolder//PythonTestFile"
+# import "..\\prototyping\\testFolder\\PythonTestFile"
+# import "prototyping\\testFolder\\PythonTestFile"
+# import "prototyping//testFolder//PythonTestFile"
+
+from testFolder import PythonTestFile
 
 PythonTestFile.test_function()

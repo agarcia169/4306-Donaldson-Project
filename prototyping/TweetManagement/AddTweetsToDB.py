2764637575c00963a3efae6c1eca363aeee0d6cc
J.Endurium <J.Endurium.K@gmail.com>
2022-10-29 17:32:15 -0500

    More tweet grabbing functions

--- prototyping/TweetManagement/AddTweetsToDB.py
+++ prototyping/TweetManagement/AddTweetsToDB.py
@@ -1,6 +1,13 @@
 from SharedConnectors import twitterConnection
 from SharedConnectors import dbConnection
 import tweepy
+from datetime import datetime as DateTime
+import datetime
+
+one_year = datetime.timedelta(days=365)
+one_year_ago = (DateTime.now() - one_year).replace(microsecond=0)
+five_years = one_year * 5
+five_years_ago = (DateTime.now() - five_years).replace(microsecond=0)
 
 def add_tweet_to_db(thisTweet:tweepy.Tweet):
     thisDBClient = dbConnection.get_db_connection()
@@ -16,7 +23,7 @@ def add_tweet_to_db(thisTweet:tweepy.Tweet):
 def retrieve_recent_tweets(theUserID, *, end_time=None, exclude=['retweets', 'replies'], 
                             expansions=None, #['author_id', 'in_reply_to_user_id'], 
                             max_results=None, media_fields=None, pagination_token=None, 
-                            place_fields=None, poll_fields=None, since_id=None, start_time=None, 
+                            place_fields=None, poll_fields=None, since_id=None, start_time=five_years_ago.isoformat()+'Z', 
                             tweet_fields=['author_id', 'conversation_id', 'created_at', 'in_reply_to_user_id', 'lang', 'text'], 
                             until_id=None, user_fields=None):
     thisDBClient = dbConnection.get_db_connection()
@@ -29,14 +36,28 @@ def retrieve_recent_tweets(theUserID, *, end_time=None, exclude=['retweets', 're
                             'pagination_token':pagination_token, 'place_fields':place_fields, 
                             'poll_fields':poll_fields, 'since_id':theMostRecentTweetID, 'start_time':start_time, 
                             'tweet_fields':tweet_fields, 'until_id':until_id, 'user_fields':user_fields}
-    retrieve_tweets(theUserID, **argumentDictionary)
+    retrieve_tweets(theUserID, argumentDictionary)
 
-def retrieve_tweets(theUserID, *, end_time=None, exclude=['retweets', 'replies'], 
+def retrieve_older_tweets(theUserID, *, end_time=None, exclude=['retweets', 'replies'], 
                             expansions=None, #['author_id', 'in_reply_to_user_id'], 
                             max_results=None, media_fields=None, pagination_token=None, 
-                            place_fields=None, poll_fields=None, since_id=None, start_time=None, 
+                            place_fields=None, poll_fields=None, since_id=None, start_time=five_years_ago.isoformat()+'Z', 
                             tweet_fields=['author_id', 'conversation_id', 'created_at', 'in_reply_to_user_id', 'lang', 'text'], 
                             until_id=None, user_fields=None):
+    print(start_time)
+    thisDBClient = dbConnection.get_db_connection()
+    the_oldest_tweet_id_query = "SELECT MIN(id) FROM tweets WHERE author_id = (%s)"
+    with thisDBClient.cursor() as dbCursor: #grab the ID of the oldest tweet currently in the database.
+        dbCursor.execute(the_oldest_tweet_id_query,(theUserID,))
+        theMostRecentTweetID = dbCursor.fetchone()[0]
+    argumentDictionary = {'end_time':end_time, 'exclude':exclude, 'expansions':expansions, 
+                            'max_results':max_results, 'media_fields':media_fields, 
+                            'pagination_token':pagination_token, 'place_fields':place_fields, 
+                            'poll_fields':poll_fields, 'since_id':since_id, 'start_time':start_time, 
+                            'tweet_fields':tweet_fields, 'until_id':theMostRecentTweetID, 'user_fields':user_fields}
+    retrieve_tweets(theUserID, argumentDictionary)
+
+def retrieve_tweets(theUserID, argumentDictionary):
     thisTwitterClient = twitterConnection.get_twitter_connection()
     thisDBClient = dbConnection.get_db_connection()
     # max_results=5 # REMOVE LATER
@@ -45,21 +66,23 @@ def retrieve_tweets(theUserID, *, end_time=None, exclude=['retweets', 'replies']
     # tracker = 0
     for thisResponse in tweepy.Paginator(thisTwitterClient.get_users_tweets, theUserID, **argumentDictionary):
         # tracker+=1
-        thisTweet:tweepy.Tweet
-        for thisTweet in thisResponse.data:
-            # print("Type:", type(thisTweet))
-            # print("DIR:", dir(thisTweet))
-            # print("Data:", thisTweet.data)
-            # print("Author_id:", thisTweet.author_id)
-            add_tweet_to_db(thisTweet=thisTweet)
-            # print(hasattr(thisTweet,'author_id'))
-            # print("Data DIR:", dir(thisTweet.data))
-            # print("Includes:", thisTweet.includes)
-            # print("Includes DIR:", dir(thisTweet.includes))
-            # print("Meta:", thisTweet.meta)
-            # print("Index:", dir(thisTweet.index))
-            # print("count:", dir(thisTweet.count))
-            # print("Errors:", thisTweet.errors)
+        # print(thisResponse)
+        if(thisResponse.data != None):
+            thisTweet:tweepy.Tweet
+            for thisTweet in thisResponse.data:
+                # print("Type:", type(thisTweet))
+                # print("DIR:", dir(thisTweet))
+                # print("Data:", thisTweet.data)
+                # print("Author_id:", thisTweet.author_id)
+                add_tweet_to_db(thisTweet=thisTweet)
+                # print(hasattr(thisTweet,'author_id'))
+                # print("Data DIR:", dir(thisTweet.data))
+                # print("Includes:", thisTweet.includes)
+                # print("Includes DIR:", dir(thisTweet.includes))
+                # print("Meta:", thisTweet.meta)
+                # print("Index:", dir(thisTweet.index))
+                # print("count:", dir(thisTweet.count))
+                # print("Errors:", thisTweet.errors)
 
         # if(tracker >= limitBreak):
         #     raise Exception

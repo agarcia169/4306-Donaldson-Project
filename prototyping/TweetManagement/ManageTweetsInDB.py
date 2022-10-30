e1fb873a1784f295e2a3401c9da67bdac3a026ad
Alexander Garcia <agarcia169@angelo.edu>
2022-10-30 16:44:03 -0500

    updated manageTweetinDB methods

---
+++ prototyping/TweetManagement/ManageTweetsInDB.py
@@ -0,0 +1,41 @@
+
+
+#CHECK IF JOEL WENT AND DID ALL OF THIS ALREADY
+
+
+#think hard about how we want to remove tweets here
+def remove_tweets_from_database(int):
+    #int param was initially for the deletion of tweet with int id
+
+
+def remove_tweets_from_database(int):
+
+
+def return_tweets_from_company(int|str):
+
+
+def return_tweets_from_lang(int|str):
+
+
+def return_tweets_from_date_range(datetime,datetime):
+
+
+def return_tweet_contents(int):
+    
+
+def return_tweet_lang(int):
+
+
+def return_tweet_author(int):
+
+
+def return_tweet_date(int):
+
+
+def add_tweet_to_database(int):
+
+
+def add_tweets_from_author(str|int,int):
+
+
+def add_tweets_from_author_from_date_range(str|int,int,datetime,datetime):

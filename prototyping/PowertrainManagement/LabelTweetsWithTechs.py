8b28e8aa8aa1c6d6019f573db2d19adadff980f3
Adebolanle Balogun <abalogun2@angelo.edu>
2022-10-30 23:26:31 -0500

    added more to labelwithtweets and started companypowertrains

--- prototyping/PowertrainManagement/LabelTweetsWithTechs.py
+++ prototyping/PowertrainManagement/LabelTweetsWithTechs.py
@@ -3,14 +3,14 @@ from SharedConnectors import dbConnection
 
 def evaluate_new_tweets():
 	thisDBClient = dbConnection.get_db_connection()
-	with thisDBClient.cursor() as dbCursor:#grab the ID of the newest tweet currently in the database.
+	with thisDBClient.cursor() as dbCursor
 		keywB ="Select word FROM battElec" #grabs list of words from battElec
 		keywCell ="Select word FROM hfuelcell"
 		keywNat ="Select word FROM natgas"
 		keywH ="Select word FROM hce"
 		#select,word,from name of table
-		revaluate_all_tweets_query = "SELECT id, text FROM tweets WHERE powertrain_set is null"
-		grabbatterytweets_query ="SELECT id, text FROM tweets WHERE text LIKE '%battery%'"
+		revaluate_all_tweets_query = "SELECT id, text FROM tweets WHERE powertrain_set is null" #checking for tweets that haven't been evaluated
+		grabbatterytweets_query ="SELECT id, text FROM tweets WHERE text LIKE '%battery%'" #grabbing tweets that have mentioned battery
 		cpare_query = "SELECT word, text FROM tweets WHERE text LIKE '%battery%' = word FROM battElec" #compares the tweets that has battery in them to the contents of battElec
 		#dbCursor.execute(revaluate_all_tweets_query)
 		dbCursor.execute(grabbatterytweets_query)
@@ -27,7 +27,10 @@ def evaluate_new_tweets():
 
 
 def updatelabels():
-	updatelabels_query = " UPDATE powertrain_set SET battElec WHERE text LIKE '%battery%'" #marks tweets that have battery in them with battElec ?
+	updatebattElec_query = " UPDATE powertrain_set SET battElec WHERE text LIKE '%battery%'" #marks tweets that have battery in them with battElec ?
+	updatehfuelcell_query = " UPDATE powertrain_set SET hfuelcell WHERE text LIKE '%%hydrogen%fuel%cell%%'"
+	updatenatgas_query = " UPDATE powertrain_set SET natgas WHERE text LIKE '%natural%%gas%'"
+	updatehce_query = " UPDATE powertrain_set SET hce WHERE text LIKE '%energy%'"
 	dbCursor.execute(updatelabels_query)
 	#update labels to show if tweet is tech related or not
 

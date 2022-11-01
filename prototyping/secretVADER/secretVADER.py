a9bcfd922d2a4b3bbf14aa1b21933f12dfd471d0
J.Endurium <J.Endurium.K@gmail.com>
2022-11-01 01:14:01 -0500

    Final tweaks

--- prototyping/secretVADER/secretVADER.py
+++ prototyping/secretVADER/secretVADER.py
@@ -8,10 +8,10 @@ def test_secret_VADER_slow_and_bad():
 
     grab_unVADERed_tweets_query = 'SELECT id, text FROM tweets WHERE VADERcompound IS NULL'
     update_tweet_with_VADER_scores = 'UPDATE tweets SET VADERneg = %s, VADERneu = %s, VADERpos = %s, VADERcompound = %s WHERE id = %s'
-    update_temp_table = 'INSERT INTO tempVader VALUES %s'
+    # update_temp_table = 'INSERT INTO tempVader VALUES %s'
     # https://stackoverflow.com/questions/1262786/mysql-update-query-based-on-select-query
 
-    merge_temp_and_tweets = 'UPDATE tweets AS destinationTable INNER JOIN tempVADER AS sourceTable ON destinationTable.id = sourceTable.id SET destinationTable.VADERcompound = sourceTable.compound, destinationTable.VADERneg = sourceTable.nega, destinationTable.VADERneu = sourceTable.neu, destinationTable.VADERpos = sourceTable.pos'
+    # merge_temp_and_tweets = 'UPDATE tweets AS destinationTable INNER JOIN tempVADER AS sourceTable ON destinationTable.id = sourceTable.id SET destinationTable.VADERcompound = sourceTable.compound, destinationTable.VADERneg = sourceTable.nega, destinationTable.VADERneu = sourceTable.neu, destinationTable.VADERpos = sourceTable.pos'
     # update_duplicate_trickery = 'INSERT INTO tweets(id, VADERneg, VADERneu, VADERpos, VADERcompound) VALUES %s AS new(newid, nega, neu, pos, compound) ON DUPLICATE KEY UPDATE VADERneg = nega, VADERneu = neu, VADERpos = pos, VADERcompound = compound'
     # ^ nega not neg. Huh. Weird.
     # Nah, need to have defaults for non-nulls such as author_id. Just do a temporary table and move data over.
@@ -49,7 +49,7 @@ def test_secret_VADER():
     VADERAnalyzer = SentimentIntensityAnalyzer()
 
     grab_unVADERed_tweets_query = 'SELECT id, text FROM tweets WHERE VADERcompound IS NULL'
-    update_tweet_with_VADER_scores = 'UPDATE tweets SET VADERneg = %s, VADERneu = %s, VADERpos = %s, VADERcompound = %s WHERE id = %s'
+    # update_tweet_with_VADER_scores = 'UPDATE tweets SET VADERneg = %s, VADERneu = %s, VADERpos = %s, VADERcompound = %s WHERE id = %s'
     update_temp_table = 'INSERT INTO tempVader VALUES %s'
     # https://stackoverflow.com/questions/1262786/mysql-update-query-based-on-select-query
 

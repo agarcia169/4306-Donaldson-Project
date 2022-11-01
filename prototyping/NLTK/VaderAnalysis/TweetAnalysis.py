1b327f0035559e8c094f0b3cdc2385fc2770a64d
Alexander Garcia <agarcia169@angelo.edu>
2022-11-01 00:23:20 -0500

    update where vader analysis works with single id

--- prototyping/NLTK/VaderAnalysis/TweetAnalysis.py
+++ prototyping/NLTK/VaderAnalysis/TweetAnalysis.py
@@ -2,38 +2,29 @@ from SharedConnectors import dbConnection
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from nltk import tokenize
 
-
-#needs a new def for 
-
+ 
 # the def needs to have a list passed in as an arg that has all the DBTweet contents
 def analyze_analyzed_tweets_in_DB():
     thisDBClient = dbConnection.get_db_connection()
     query_TweetList = "SELECT * FROM tweets WHERE id = (%s)"
     with thisDBClient.cursor() as dbCursor: 
-        dbCursor.execute(query_TweetList,(928073445941895168,))
-        TweetList = dbCursor.fetchone()
+        dbCursor.execute(query_TweetList,(935308793788948481,))
+        TweetList = dbCursor.fetchall()
     sentences = []
     #for tweet in TweetList: 
-    sentences.append(TweetList[2])
+    sentences.append(TweetList[0][2])
     for sentence in sentences:
         sid = SentimentIntensityAnalyzer()
         #figure out how to seperate posnegneu reviews
         ss = sid.polarity_scores(sentence)
         print(ss)
-        #this sentence selector might not work idfk
-        # TweetList[sentence].VADERcompound = ss[0]
-        # TweetList[sentence].VADERneg = ss[1]
-        # TweetList[sentence].VADERneu = ss[2]
-        # TweetList[sentence].VADERpos = ss[3]
-
-        
-        # for thisTweet in TweetList:
-        #     query_add_vader_results_to_db = """UPDATE tweets SET (VADERcompound, VADERneg, VADERneu, VADERpos) 
-        #     VALUES(%s,%s,%s,%s)  WHERE id = %s)"""
-        #     with thisDBClient.cursor() as dbCursor:
-        #         dbCursor.execute(query_add_vader_results_to_db,(thisTweet.VADERcompound, thisTweet.VADERneg, thisTweet.VADERneu, thisTweet.VADERpos, thisTweet.id))
-        #         dbCursor.fetchall()
-        #     thisDBClient.commit()
+        for thisTweet in TweetList:
+            query_add_vader_results_to_db = """UPDATE tweets SET VADERcompound = %s, VADERneg = %s, VADERneu = %s, VADERpos = %s
+              WHERE id = %s"""
+            with thisDBClient.cursor() as dbCursor:
+                dbCursor.execute(query_add_vader_results_to_db,(ss["compound"], ss["neg"], ss["neu"], ss["pos"], 935308793788948481))
+                dbCursor.fetchall()
+            thisDBClient.commit()
     #PseudoCode
     #==================
     #1.parse list arg into sentences 

955e34eaf78443e0da388f193c6ea771e9ebe6e2
Joel K <91800518+JEnduriumK@users.noreply.github.com>
2022-11-04 12:03:28 -0500

    Move queries into dbConnection

--- prototyping/NLTK/VaderAnalysis/TweetAnalysis.py
+++ prototyping/NLTK/VaderAnalysis/TweetAnalysis.py
@@ -4,15 +4,14 @@ from nltk import tokenize
 
  
 def analyze_analyzed_tweets_in_DB():
-    TweetsToAnalyze = "SELECT * FROM tweets WHERE VADERcompound is null LIMIT 0,10000"
+    TweetsToAnalyze = dbConnection.query_TweetsToAnalyze
     thisDBClient = dbConnection.get_db_connection()
     #UNUSED var
     #query_TweetList = "SELECT * FROM tweets WHERE id = (%s)"
     with thisDBClient.cursor() as dbCursor:
         dbCursor.execute(TweetsToAnalyze)
         TweetList = dbCursor.fetchall()
-    query_add_vader_results_to_db = """UPDATE tweets SET VADERcompound = %s, VADERneg = %s, VADERneu = %s, VADERpos = %s
-    WHERE id = %s"""
+    query_add_vader_results_to_db = dbConnection.query_add_vader_results_to_db_ComNegNeuPosID
     sentences = []
     for tweet in TweetList: 
         sid = SentimentIntensityAnalyzer()
--- prototyping/PowertrainManagement/LabelTweetsWithTechs.py
+++ prototyping/PowertrainManagement/LabelTweetsWithTechs.py
@@ -4,14 +4,14 @@ from SharedConnectors import dbConnection
 def evaluate_new_tweets():
 	thisDBClient = dbConnection.get_db_connection()
 	with thisDBClient.cursor() as dbCursor:
-		keywB ="Select word FROM battElec" #grabs list of words from battElec
-		keywCell ="Select word FROM hfuelcell"
-		keywNat ="Select word FROM natgas"
-		keywH ="Select word FROM hce"
+		keywB = dbConnection.query_keywB #grabs list of words from battElec
+		keywCell = dbConnection.query_keywCell
+		keywNat = dbConnection.query_keywNat
+		keywH = dbConnection.query_keywH
 		#select,word,from name of table
-		revaluate_all_tweets_query = "SELECT id, text FROM tweets WHERE powertrain_set is null" #checking for tweets that haven't been evaluated
-		grabbatterytweets_query ="SELECT id, text FROM tweets WHERE text LIKE '%battery%'" #grabbing tweets that have mentioned battery
-		cpare_query = "SELECT word, text FROM tweets WHERE text LIKE '%battery%' = word FROM battElec" #compares the tweets that has battery in them to the contents of battElec
+		revaluate_all_tweets_query = dbConnection.query_revaluate_all_tweets
+		grabbatterytweets_query = dbConnection.query_grabbatterytweets
+		cpare_query = dbConnection.query_cpare
 		#dbCursor.execute(revaluate_all_tweets_query)
 		dbCursor.execute(grabbatterytweets_query)
 		#dbCursor.execute(cpare_query)
@@ -29,7 +29,7 @@ def evaluate_new_tweets():
 def updatelabels():
 	thisDBClient = dbConnection.get_db_connection()
 	with thisDBClient.cursor() as dbCursor:	
-		updatebattElec_query = "UPDATE tweets SET powertrain_set = CONCAT_WS(',',powertrain_set,'battElec') WHERE (text Like '%battery%' or text Like '%Lithium%' or text Like '%batteries%' or text Like '%electric%' or text Like '%electrify%') "#marks tweets that have battery in them with battElec ?
+		updatebattElec_query = dbConnection.query_updatebattElec
 		#updatebattElec_query2 = """
 		#	UPDATE tweets #tweet is the table
 		#	SET 
@@ -37,9 +37,9 @@ def updatelabels():
 		#	WHERE
 		#		text LIKE '%battery%';"""
 		#updatebattElec_query = "UPDATE tweets SET powertrain_set = CONCAT(powertrain_set,",'battElec'") WHERE text in('battery','Lithium')"#marks tweets that have battery in them with battElec ?	
-		updatehfuelcell_query = "UPDATE tweets SET powertrain_set = CONCAT_WS(',',powertrain_set,'hfuelcell') WHERE (text Like '%hydrogen%fuel%cell%')"
-		updatenatgas_query = "UPDATE tweets SET powertrain_set = CONCAT_WS(',',powertrain_set,'natgas') WHERE (text Like '%natural%gas%')"
-		updatehce_query = "UPDATE tweets SET powertrain_set = CONCAT_WS(',',powertrain_set,'hce') WHERE (text Like '%battery%' or text Like '%Lithium%') "#what exactly is hce ??
+		updatehfuelcell_query = dbConnection.query_updatehfuelcell
+		updatenatgas_query = dbConnection.query_updatenatgas
+		updatehce_query = dbConnection.query_updatehce
 		dbCursor.execute(updatebattElec_query)
 		thisDBClient.commit()#hopefully would commit the query showing the powertrain set for those tweets
 		#update labels to show if tweet is tech related or not

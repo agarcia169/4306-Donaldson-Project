732b90af67413cf68c398920a2ab8bb4651b28e3
Alexander Garcia <agarcia169@angelo.edu>
2022-11-06 16:38:44 -0600

    update .5 on that last one?

---
+++ prototyping/Reporting&UI/CSVOutput.py
@@ -0,0 +1,10 @@
+from SharedConnectors import dbConnection
+  
+def dumpy ():
+    thisDBClient = dbConnection.get_db_connection()
+    query_create_csv = dbConnection.query_csv_creation
+    with thisDBClient.cursor() as dbCursor:
+        dbCursor.execute(query_create_csv)
+        dbCursor.fetchall()
+    
+

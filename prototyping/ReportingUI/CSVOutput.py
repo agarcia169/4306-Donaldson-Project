81e3929a7777b1e9efa2b2f404b6adf9202ef3b3
Alexander Garcia <agarcia169@angelo.edu>
2022-11-06 18:09:38 -0600

    finished CSVOutput

--- prototyping/ReportingUI/CSVOutput.py
+++ prototyping/ReportingUI/CSVOutput.py
@@ -1,5 +1,6 @@
 from SharedConnectors import dbConnection
 from itertools import chain
+import csv
 
 def dumpy():
     thisDBClient = dbConnection.get_db_connection()
@@ -7,10 +8,16 @@ def dumpy():
     with thisDBClient.cursor() as dbCursor:
         dbCursor.execute(query_create_csv_string)
         table = dbCursor.fetchall()
-    table = ','.join(map(str,chain.from_iterable(table)))
-    csvfile = open('C:\\temp\\csvfile.csv', 'w',encoding="utf-8")
-    csvfile.write(table)
-    csvfile.close() 
+    #i dont think i need this
+    #table = '\t'.join(map(str,chain.from_iterable(table)))
+    with open('C:\\temp\\csvfile.csv', 'w',encoding="utf-8", newline="\n") as csvfile:
+        #do i even need delimiter?
+        writer = csv.writer(csvfile, delimiter="\t")
+        for row in table:
+            row = (row[0],row[1], row[2].replace("\n","").replace("\t",""), row[3], row[4], row[5], str(row[6]), row[7], row[8], row[9], row[10])
+            writer.writerow(row)
+    csvfile.close()
+    
 
 
 

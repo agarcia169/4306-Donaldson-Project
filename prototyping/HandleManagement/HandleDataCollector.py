512d24bb9ee4777e3bc0f6b74155b9cd889cb7c0
J.Endurium <J.Endurium.K@gmail.com>
2022-11-10 18:56:44 -0600

    Correct mistake in docstring

--- prototyping/HandleManagement/HandleDataCollector.py
+++ prototyping/HandleManagement/HandleDataCollector.py
@@ -4,13 +4,13 @@ from requests import Response
 
 
 def get_handle_from_twitter(twitter_username: str) -> dict | requests.Response | Response:
-    """Adds a Twitter ID, username, description, and name to the database.
+    """Returns a Twitter ID, username, description, and name.
 
     Args:
         `twitter_user` (`str`): The username Twitter knows the user by.
 
     Returns:
-        `tuple(bool,int)`: A bool representing whether or not the user was added successfully, and an int that is the ID# of Twitter user
+        `Response`: A Tweepy object containing the user's ID, username, description, and name.
     """
     twitConnection = twitterConnection.get_twitter_connection()
     return twitConnection.get_user(username=twitter_username, user_fields=['description'])

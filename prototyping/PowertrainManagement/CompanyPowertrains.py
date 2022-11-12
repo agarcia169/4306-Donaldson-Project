ba5695eb53ae7736c3ad23c698b133221e7a564e
Adebolanle Balogun <abalogun2@angelo.edu>
2022-11-12 16:59:31 -0600

    started on company powertrains

--- prototyping/PowertrainManagement/CompanyPowertrains.py
+++ prototyping/PowertrainManagement/CompanyPowertrains.py
@@ -7,6 +7,13 @@ from SharedConnectors import dbConnection
 # def find_all techs_by_company():
 #     thisDBClient = dbConnection.get_db_connection()
 #     with thisDBClient.cursor() as dbCursor
+thisDBClient = dbConnection.get_db_connection()
+    with thisDBClient.cursor() as dbCursor:
+    gethfuelcell_query = 'Select handles.name Fom handles = CONCAT(handles,'',''hfuelcell") WHERE label in(''hfuelcell')' ' 
+   getnatgas_query = 'Select handles.name Fom handles = CONCAT(handles,'',''natgas") WHERE label in(''natgas')'' #queries should grab all tweets from companies that are labeled as such
+    gethce_query = 'Select handles.name Fom handles = CONCAT(handles,'',''hce") WHERE label in(''hce')''
+    getbattelec_query ='Select handles.name Fom handles = CONCAT(handles,'',''battElec") WHERE label in(''batElec)''
+    dbCursor.execute(updatebattElec_query)      
 
 
 # def find_all_companys_with_tech():

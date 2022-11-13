0906e376a0dac9f8e5a3b820f92fb548a18f3158
J.Endurium <J.Endurium.K@gmail.com>
2022-11-13 16:37:11 -0600

    Pytest works!

--- tests/test_vader_basic.py
+++ tests/test_vader_basic.py
@@ -1,4 +1,4 @@
 from donaldson_asu_twitter.VaderAnalysis import TweetAnalysis
 
 def test_vader_basic_one():
-    assert TweetAnalysis.one_VADER_analysis("Trusted and reliable for everyday use.") is {'neg': 0.0, 'neu': 0.617, 'pos': 0.383, 'compound': 0.4767}
+    assert TweetAnalysis.one_VADER_analysis("Trusted and reliable for everyday use.") == {'neg': 0.0, 'neu': 0.617, 'pos': 0.383, 'compound': 0.4767}

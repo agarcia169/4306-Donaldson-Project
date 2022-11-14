a3772ab51943a3e90a8a8c979e474160ef99faea
J.Endurium <J.Endurium.K@gmail.com>
2022-11-14 11:41:42 -0600

    Shift to parametrized tests

--- tests/test_vader_basic.py
+++ tests/test_vader_basic.py
@@ -1,4 +1,11 @@
+"""Tests for the vaderAnalysis module"""
+import pytest
 from donaldson_asu_twitter.VaderAnalysis import TweetAnalysis
 
-def test_vader_basic_one():
-    assert TweetAnalysis.one_VADER_analysis("Trusted and reliable for everyday use.") == {'neg': 0.0, 'neu': 0.617, 'pos': 0.383, 'compound': 0.4767}
+@pytest.mark.parametrize("test_input, expected", [
+	('Trusted and reliable for everyday use.', {'neg': 0.0, 'neu': 0.617, 'pos': 0.383, 'compound': 0.4767})
+])
+
+def test_vader_basic_one(test_input, expected):
+	"""Testing that simple vaderAnalysis works"""
+	assert TweetAnalysis.one_VADER_analysis(test_input) == expected

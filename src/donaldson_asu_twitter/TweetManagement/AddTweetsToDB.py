4e63fc37b73df25919fde9e1037ec5f073049f80
Joel K <91800518+JEnduriumK@users.noreply.github.com>
2022-11-29 10:31:19 -0600

    Remove old code

--- src/donaldson_asu_twitter/TweetManagement/AddTweetsToDB.py
+++ src/donaldson_asu_twitter/TweetManagement/AddTweetsToDB.py
@@ -6,93 +6,6 @@ from ..SharedConnectors import twitterConnection
 from ..SharedConnectors import dbConnection
 
 
-
-# one_year = datetime.timedelta(days=365)
-# one_year_ago = (DateTime.now() - one_year).replace(microsecond=0)
-# five_years = one_year * 5
-# five_years_ago = (DateTime.now() - five_years).replace(microsecond=0)
-# how_long_to_grab = five_years_ago
-
-
-# def add_tweet_to_db(thisTweet: tweepy.Tweet):
-#     thisDBClient = dbConnection.get_db_connection()
-#     query_add_tweet_to_db = dbConnection.query_add_tweet_to_db_IDAuthTextCreateLangConvo
-#     with thisDBClient.cursor() as dbCursor:
-#         dbCursor.execute(query_add_tweet_to_db, (thisTweet.id, thisTweet.author_id, thisTweet.text,
-#                                                 thisTweet.created_at, thisTweet.lang, thisTweet.conversation_id))
-#         dbCursor.fetchall()
-#     thisDBClient.commit()
-
-
-# def retrieve_recent_tweets(theUserID, *, end_time=None, exclude=['retweets', 'replies'],
-#                            expansions=None, max_results=None, media_fields=None, pagination_token=None,
-#                            place_fields=None, poll_fields=None, since_id=None, start_time=how_long_to_grab.isoformat()+'Z',
-#                            tweet_fields=['author_id', 'conversation_id',
-#                                          'created_at', 'in_reply_to_user_id', 'lang', 'text'],
-#                            until_id=None, user_fields=None):
-
-# def retrieve_recent_tweets(theUserID:int,* , maxDaysInPast:int=365*5, **kwargs):
-# 	"""Grab any tweets since the most recently grabbed Tweet for a specified user.
-
-# 	Args:
-# 		theUserID (int): The Twitter User ID#
-# 		maxDaysInPast (int, optional): The maximum number of days in the past you want to look for Tweets. Defaults to 365*5.
-# 		**kwargs: Any tweepy.Paginator() arguments. Probably best left default.
-# 	"""
-# 	timeToGrab = datetime.timedelta(days=maxDaysInPast)
-# 	how_long_to_grab = (DateTime.now() - timeToGrab).replace(microsecond=0)
-# 	thisDBClient = dbConnection.get_db_connection()
-# 	defaultKwargs = {'exclude':['retweets', 'replies'], 
-# 						'tweet_fields':['author_id', 'conversation_id',
-# 						 'created_at', 'in_reply_to_user_id', 'lang', 'text'],
-# 						 'start_time':(how_long_to_grab.isoformat()+'Z')}
-	
-# 	# print(kwargs)
-# 	the_most_recent_tweet_id_query = dbConnection.query_the_most_recent_tweet_id
-# 	# grab the ID of the newest tweet currently in the database.
-# 	with thisDBClient.cursor() as dbCursor:
-# 		dbCursor.execute(the_most_recent_tweet_id_query, (theUserID,))
-# 		# print("Executing:\n",dbCursor.statement)
-# 		theMostRecentTweetID = dbCursor.fetchone()[0]
-# 	defaultKwargs.update(kwargs)
-# 	kwargs = defaultKwargs
-# 	kwargs.update({'since_id':theMostRecentTweetID})
-# 	# print(kwargs)
-# 	_retrieve_tweets(theUserID, **kwargs)
-
-
-# def retrieve_older_tweets(theUserID, *, end_time=None, exclude=['retweets', 'replies'],
-#                           expansions=None, max_results=None, media_fields=None, pagination_token=None,
-#                           place_fields=None, poll_fields=None, since_id=None, start_time=how_long_to_grab.isoformat()+'Z',
-#                           tweet_fields=['author_id', 'conversation_id',
-#                                         'created_at', 'in_reply_to_user_id', 'lang', 'text'],
-#                           until_id=None, user_fields=None):
-# def retrieve_older_tweets(theUserID:int, *, maxDaysInPast:int=365*5, **kwargs):
-# 	"""Grab Tweets older than the oldest Tweets grabbed so far.
-
-# 	Args:
-# 		theUserID (int): The Twitter User ID#.
-# 		maxDaysInPast (int, optional): Look no further than this many days into the past. Defaults to 365*5.
-# 	"""
-# 	timeToGrab = datetime.timedelta(days=maxDaysInPast)
-# 	how_long_to_grab = (DateTime.now() - timeToGrab).replace(microsecond=0)
-# 	thisDBClient = dbConnection.get_db_connection()
-# 	defaultKwargs = {'exclude':['retweets', 'replies'], 
-# 						'tweet_fields':['author_id', 'conversation_id',
-# 						 'created_at', 'in_reply_to_user_id', 'lang', 'text'],
-# 						 'start_time':(how_long_to_grab.isoformat()+'Z')}
-# 	the_oldest_tweet_id_query = dbConnection.query_the_oldest_tweet_id
-# 	# grab the ID of the oldest tweet currently in the database.
-# 	with thisDBClient.cursor() as dbCursor:
-# 		dbCursor.execute(the_oldest_tweet_id_query, (theUserID,))
-# 		# print("Executing:\n",dbCursor.statement)
-# 		theOldestTweetIDWeHave = dbCursor.fetchone()[0]
-# 	defaultKwargs.update(kwargs)
-# 	kwargs = defaultKwargs
-# 	kwargs.update({'until_id': theOldestTweetIDWeHave})
-# 	_retrieve_tweets(theUserID, **kwargs)
-
-
 def refresh_tweets(theUserID:int, *, maxDaysInPast:int=365*5, older_tweets:bool=True, newer_tweets:bool=True, exclude_responses:bool=True, **kwargs):
 	"""Grab any Tweets we don't have yet.
 
@@ -118,7 +31,7 @@ def refresh_tweets(theUserID:int, *, maxDaysInPast:int=365*5, older_tweets:bool=
 		dbCursor.execute(dbConnection.query_count_of_tweets_from_company,(theUserID,))
 		howManyTweetsDoTheyHave = dbCursor.fetchall()[0][0]
 	if(howManyTweetsDoTheyHave):
-		print(f'{theUserID} had {howManyTweetsDoTheyHave} Tweets')
+		print(f'{theUserID} had {howManyTweetsDoTheyHave} Tweets in the primary Tweets database')
 		if newer_tweets:
 			the_most_recent_tweet_id_query = dbConnection.query_the_most_recent_tweet_id
 			# grab the ID of the newest tweet currently in the database.
@@ -230,10 +143,8 @@ def _retrieve_tweets(theUserID, **argumentDictionary):
 		dbCursor.execute(dbConnection.query_number_of_tweet_relationships)
 		number_of_tweet_relationships_in_db_at_start = dbCursor.fetchone()[0]
 	if listOfTweets:
-		print('one')
 		mass_add_tweets_to_db(listOfTweets, database='tweets')
 	if list_of_referenced_tweets or list_of_referencing_tweets:
-		print('two')
 		how_many_duplicate_referenced_tweets = mass_add_tweets_to_db(list_of_referenced_tweets, database='referenced_tweets')
 		how_many_duplicate_retweets = mass_add_tweets_to_db(list_of_referencing_tweets, database='retweets')
 		add_relationships(list_of_tweet_relationships)

581ca8eda939b93c09fd84ee4293063e32af1259
J.Endurium <J.Endurium.K@gmail.com>
2022-12-05 22:44:27 -0600

    A few comments

--- src/donaldson_asu_twitter/ReportingUI/dataGathering.py
+++ src/donaldson_asu_twitter/ReportingUI/dataGathering.py
@@ -1,24 +1,36 @@
 from ..SharedConnectors import dbConnection
 
 def get_pos_neg_scores(*,company_id:int|list[int]|tuple[int]=None) -> tuple[list]:
-    singleCompany = isinstance(company_id,int)
+    # Is this a single integer/company?
+    singleCompany = isinstance(company_id,int) 
+    # Or a group of companies?
     companyGroup = isinstance(company_id,list) or isinstance(company_id,tuple)
+    # If we were handed a list, lets make it a tuple.
     if isinstance(company_id,list):
         company_id = (*company_id,)
+    
+    #Prep some holding areas for data and dates.
     posScatterBattElec = []
     negScatterBattElec = []
     battElecDates = []
+
+    # The start of the query. Select pos, neg, and dates...
     scatter_battelec_pair = dbConnection.query_vlines_battelec
+
+    # If we want to limit it to a company or companies...
     if company_id is not None:
         if singleCompany:
+            # Tack on the filter.
             scatter_battelec_pair += " " + dbConnection.query_author_filter
         elif companyGroup:
+            # Tack on a piece of the filter and...
             scatter_battelec_pair += dbConnection.query_and + "("
             for _ in company_id:
+                # Tack on more pieces for each company
                 scatter_battelec_pair += dbConnection.query_author_piecemeal_filter + dbConnection.query_or
+            # Trim the hanging OR
             scatter_battelec_pair = scatter_battelec_pair.removesuffix(dbConnection.query_or)
             scatter_battelec_pair += ")"
-            print(scatter_battelec_pair)
     thisDBClient = dbConnection.get_db_connection()
     with thisDBClient.cursor() as dbCursor:
         if company_id is None:
